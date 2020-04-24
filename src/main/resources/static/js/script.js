6d406b80f42a4007338dae620d1e3eca9d5d2782
Kishore8 <sai.kishore2015@gmail.com>
2020-04-24 12:13:16 +0530

    private messaging by getting all users

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,6 +5,7 @@ var stompClient = null;
 var name = null;
 var receiver = null;
 var listOfActiveUsers = [];
+var selectedUser = '';
 const url = 'http://localhost:8080';
 function connect(event) {
 
@@ -71,7 +72,7 @@ function sendMessage(event) {
 			type : 'CHAT',
 	};
 
-	stompClient.send("/app/chat/" + "supreet", {}, JSON.stringify({
+	stompClient.send("/app/chat/" + selectedUser, {}, JSON.stringify({
             fromLogin: name,
             message: document.querySelector('#chatMessage').value
         }));
@@ -110,4 +111,41 @@ function onMessageReceived(payload) {
 	document.querySelector('#messageList').appendChild(messageElement);
 	document.querySelector('#messageList').scrollTop = document
 	.querySelector('#messageList').scrollHeight;
+}
+
+
+function selectUser(userName) {
+    console.log("selecting users: " + userName);
+    selectedUser = userName;
+//    let isNew = document.getElementById("newMessage_" + userName) !== null;
+//    if (isNew) {
+//        let element = document.getElementById("newMessage_" + userName);
+//        element.parentNode.removeChild(element);
+//        render(newMessages.get(userName), userName);
+//    }
+
+    $('#selectedUserId').html('');
+    $('#selectedUserId').append('Chat with ' + userName);
+}
+
+
+
+    function fetchAll() {
+        $.get(url + "/fetchAllUsers", function (response) {
+            let users = response;
+            let usersTemplateHTML = "";
+            for (let i = 0; i < users.length; i++) {
+                usersTemplateHTML = usersTemplateHTML + '<a href="#" onclick="selectUser(\'' + users[i] + '\')"><li class="clearfix">\n' +
+                    '                <img src="https://rtfm.co.ua/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png" width="55px" height="55px" alt="avatar" />\n' +
+                    '                <div class="about">\n' +
+                    '                    <div id="userNameAppender_' + users[i] + '" class="name">' + users[i] + '</div>\n' +
+                    '                    <div class="status">\n' +
+                    '                        <i class="fa fa-circle offline"></i>\n' +
+                    '                    </div>\n' +
+                    '                </div>\n' +
+                    '            </li></a>';
+            }
+            $('#usersList').html(usersTemplateHTML);
+        });
+
 }
